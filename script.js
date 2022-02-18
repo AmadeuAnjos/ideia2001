fc5145a81f40f295283f6909f823ca86a7226334
AmadeAnjos <anjosbarrosclean@gmail.com>
2022-02-17 21:09:32 -0300

    criando interacao nos botoes com JS

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-function showModal() {
-    var element = document.getElementById("modal");
-    element.classList.add("show-modal")
+function evento() {
+   
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'block';
+
 }
 
-function hideModal() {
-    var element = document.getElementById("modal");
-    element.classList.remove("show-modal")
+function fechar() {
+   
+    let modal = document.querySelector('.modal')
+
+    modal.style.display = 'none';
+
+}
+function evento2() {
+   
+    let modal = document.querySelector('.modal2')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar2() {
+   
+    let modal = document.querySelector('.modal2')
+
+    modal.style.display = 'none';
+
+}
+function evento3() {
+   
+    let modal = document.querySelector('.modal3')
+
+    modal.style.display = 'block';
+
+}
+
+function fechar3() {
+   
+    let modal = document.querySelector('.modal3')
+
+    modal.style.display = 'none';
+
 }
