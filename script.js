83f058c502d50bbf3f4f652b6e081e618492fc16
AmadeAnjos <anjosbarrosclean@gmail.com>
2022-02-18 00:27:39 -0300

    implementando responsividade

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
+/* funcoes para chamar e fechar modal, modal2 e modal3 */
 function evento() {
    
     let modal = document.querySelector('.modal')
-
     modal.style.display = 'block';
 
 }
 
 function fechar() {
-   
-    let modal = document.querySelector('.modal')
 
+    let modal = document.querySelector('.modal')
     modal.style.display = 'none';
 
 }
 function evento2() {
    
     let modal = document.querySelector('.modal2')
-
     modal.style.display = 'block';
 
 }
@@ -24,14 +22,12 @@ function evento2() {
 function fechar2() {
    
     let modal = document.querySelector('.modal2')
-
     modal.style.display = 'none';
 
 }
 function evento3() {
    
     let modal = document.querySelector('.modal3')
-
     modal.style.display = 'block';
 
 }
@@ -39,7 +35,7 @@ function evento3() {
 function fechar3() {
    
     let modal = document.querySelector('.modal3')
-
     modal.style.display = 'none';
 
 }
+
